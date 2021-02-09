518d08771f733eed599cd23652dd188305988185
hicham gamany <hgamany@gmail.com>
2021-02-09 21:42:09 +0100

    Initial Commit

---
+++ Converter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Converter
+//
+//  Created by hicham on 9/2/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
